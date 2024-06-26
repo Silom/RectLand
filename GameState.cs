edc6a2dfe989886a2a216cfb86fafe2f93dda3d0
Valentin Wetenkamp <valentin.wetenkamp@googlemail.com>
2024-06-26 14:27:47 +0200

    amend for sqlite

--- GameState.cs
+++ GameState.cs
@@ -4,6 +4,7 @@ namespace KingdomInvader
 {
     partial class GameState : Node
     {
+        public static GameSettings Settings = new();
         public static float UnitSliderValue { get; set; }
         public static Map MapNode { get; set; }
     }
